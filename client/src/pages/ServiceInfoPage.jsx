b8602a8234653174eee5af1bcae2c72a834faaad
adeshM07 <adesh.m@vedapixel.com>
2025-12-29 14:20:04 +0530

    fixed why vedapixel part fixed for smaller screens

--- client/src/pages/ServiceInfoPage.jsx
+++ client/src/pages/ServiceInfoPage.jsx
@@ -1252,7 +1252,7 @@ const ServiceInfoPage = () => {
           </p>
         </div>
         <div className="lg:w-[73vw]  mt-[80px] md:mt-[40px] overflow-hidden">
-          <p className="service-desc  text-[1.5rem] md:text-[1.8rem] text-center md:text-start  text-[#C8C1C1] mb-4">
+          <p className="service-desc  text-[1.5rem] md:text-[1.8rem] text-start  text-[#C8C1C1] mb-4">
             Technologies We Use
           </p>
 
@@ -1312,7 +1312,7 @@ const ServiceInfoPage = () => {
 
         {/* </div> */}
         <div className="lg:w-[73vw] mt-[100px]   flex flex-col gap-[0.8rem]">
-          <p className="service-title text-center  md:text-start text-[2.5rem] md:text-[3rem] w-[90vw] sm:w-[42vw] md:w-[30vw] lg:w-[22vw]  text-white">
+          <p className="service-title text-start text-[2.5rem] md:text-[3rem] w-[90vw] sm:w-[42vw] md:w-[30vw] lg:w-[22vw]  text-white">
             Our Expertise
           </p>
           <div className="">
@@ -1351,7 +1351,7 @@ const ServiceInfoPage = () => {
             Why VedaPixel ?
           </p>
           <div
-            className="w-[90vw] h-[20vh] md:w-[72vw] flex place-items-center px-7 md:px-14 md:h-[42vh] lg:h-[21vh] xl:h-[40vh] [@media(min-width:2550px)]:h-[20vh] border-2 rounded-b-[80px] rounded-tr-[80px]"
+            className="w-[90vw] min-h-[20vh] md:w-[72vw] flex place-items-center px-7 md:px-14 md:min-h-[42vh] lg:min-h-[21vh] xl:min-h-[40vh] [@media(min-width:2550px)]:min-h-[20vh] border-2 rounded-b-[80px] rounded-tr-[80px]"
             style={{
               background:
                 "linear-gradient(139.47deg, rgba(47, 54, 64, 0.8) -45.69%, rgba(16, 24, 32, 0.8) 54.7%)",
@@ -1365,7 +1365,7 @@ const ServiceInfoPage = () => {
               {serviceInfo[service]?.whyUs.map((value, index) => (
                 <li
                   key={index}
-                  className="service-desc [@media(min-width:300px)_and_(max-width:450px)]:text-[0.6rem] text-[0.8rem] md:text-[1.5rem] text-[#C8C1C1]"
+                  className="service-desc [@media(min-width:300px)_and_(max-width:450px)]:text-[0.5rem] text-[0.8rem] md:text-[1.5rem] text-[#C8C1C1]"
                 >
                   {value}
                 </li>
